4a7a085c93ec4b99fe0baff719d45def338ed421
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-06-20 03:03:47 -0700

    Delete website.php
    needed deleting

--- website.php
+++
@@ -1,9 +0,0 @@
-<?php
-$uploadDir = 'uploads/';
-$uploadedFiles = $_FILES['files'];
-
-foreach ($uploadedFiles['tmp_name'] as $key => $tmp_name) {
-  $uploadFile = $uploadDir . basename($uploadedFiles['name'][$key]);
-  move_uploaded_file($tmp_name, $uploadFile);
-}
-?>
